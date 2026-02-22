a7cf88ac3250bac0920e2360709369078c97686a
syedmhamil <syedmahamil56@gmail.com>
2026-02-22 20:03:24 +0500

    add-shedule-booking

--- src/repositories/booking.repo.js
+++ src/repositories/booking.repo.js
@@ -1,4 +1,5 @@
 import { db } from "../db/index.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const reserved_seat_booking = async (event_seat_id,
     user_id) => {
@@ -13,4 +14,22 @@ const reserved_seat_booking = async (event_seat_id,
 }
 
 
-export default {reserved_seat_booking}
+const update_booking_status = async () => {
+  try {
+
+     const  query= `
+           UPDATE bookings
+           SET status = 'expired'
+           WHERE status = 'pending' 
+           AND  expires_at < NOW()
+         `
+    const {rows} = await db.query(query)
+   return rows[0]
+    
+  } catch (error) {
+    throw new ApiError(406,error)
+  }
+} 
+
+
+export default {reserved_seat_booking,update_booking_status}
