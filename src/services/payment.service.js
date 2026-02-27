c7cae4e775dd0cc5abb805b423969a4412468051
syedmhamil <syedmahamil56@gmail.com>
2026-02-27 20:09:15 +0500

    add

---
+++ src/services/payment.service.js
@@ -0,0 +1,27 @@
+﻿import { ApiError } from "../utils/ApiError.js";
+import Stripe from "stripe";
+
+
+
+
+
+const stripe= new Stripe(process.env.STRIPE_KEY)
+
+const createPaymentIntent = async (req,res) => {
+  const {amount,currency} = req.body;
+  const {booking_id} = req.params
+
+  const paymentIntent = await stripe.paymentIntents.create({
+  amount:amount,
+  currency: currency,
+  automatic_payment_methods: {
+    enabled: true,
+  },
+  metadata: {
+    booking_id: booking_id
+  },
+});
+   
+return paymentIntent
+ 
+}
