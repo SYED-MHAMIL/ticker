588a16ced9697798bc04dd6448f903365e20ff67
syedmhamil <syedmahamil56@gmail.com>
2026-03-09 00:12:23 +0500

    dd

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-const DB_NAME ='ticketprime'
-const HOST_NAME ="host.docker.internal" 
-const USER ='postgres'
+const DB_NAME = "ticketprime";
+const HOST_NAME = "host.docker.internal";
+const USER =  "postgres";
 
-export {DB_NAME,HOST_NAME,USER}
+export { DB_NAME, HOST_NAME, USER };
